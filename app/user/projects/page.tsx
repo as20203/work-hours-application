5c5984104430a6362bf67037933729c1bf58438f
Jawad Zaheer <jzaheer.bese16seecs@seecs.edu.pk>
2024-03-12 15:46:22 +0200

    Added fixes to table navigation.

--- app/user/projects/page.tsx
+++ app/user/projects/page.tsx
@@ -86,7 +86,22 @@ export default function Home() {
                             <nav aria-label="Page navigation example">
                                 <ul className="flex items-center -space-x-px h-8 text-sm">
                                     <li>
-                                        <Link onClick={() => setPageNumber(pageNumber => pageNumber == 1 ? pageNumber : pageNumber - 1)} href="#" className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                                        <Link onClick={() => setPageNumber(pageNumber => pageNumber == 1 ? pageNumber : pageNumber - 1)} href="#"
+                                            className={`flex items-center 
+                                            justify-center 
+                                            px-3 h-8 ms-0 
+                                            leading-tight 
+                                            text-gray-500 
+                                            bg-white border border-e-0 
+                                            border-gray-300 rounded-s-lg 
+                                            dark:bg-gray-800 
+                                            dark:border-gray-700                                        
+                                            ${pageNumber !== 1 && 'hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white hover:text-gray-700'}
+                                            ${pageNumber == 1 && 'cursor-not-allowed'}
+                                            `
+                                            }
+
+                                        >
                                             <span className="sr-only">Previous</span>
                                             <svg className="w-2.5 h-2.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
@@ -95,12 +110,39 @@ export default function Home() {
                                     </li>
                                     {
                                         paginationArray.map(pageNo => <li key={pageNo}>
-                                            <Link onClick={() => setPageNumber(pageNo)} href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">{pageNo}</Link>
+                                            <Link key={pageNo} onClick={() => setPageNumber(pageNo)} href="#" className={`
+                                            flex items-center 
+                                            justify-center 
+                                            px-3 h-8 
+                                            leading-tight 
+                                            text-gray-500
+                                            ${pageNo == pageNumber
+                                                    ?
+                                                    'bg-gray-600' :
+                                                    'dark:bg-gray-800 dark:border-gray-700  border  border-gray-300'} 
+                                            hover:bg-gray-100 hover:text-gray-700 
+                                            dark:text-gray-400 
+                                            dark:hover:bg-gray-700 dark:hover:text-white`
+                                            }>{pageNo}</Link>
                                         </li>
                                         )
                                     }
                                     <li>
-                                        <Link onClick={() => setPageNumber(pageNumber => pageNumber == totalPages ? pageNumber : pageNumber + 1)} href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                                        <Link onClick={() => setPageNumber(pageNumber => pageNumber == totalPages ? pageNumber : pageNumber + 1)}
+                                            href="#"
+                                            className={`
+                                            flex items-center 
+                                            justify-center px-3 h-8 
+                                            leading-tight text-gray-500 
+                                            bg-white border border-gray-300 
+                                            rounded-e-lg
+                                            dark:bg-gray-800
+                                            ${pageNumber != totalPages && 'hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white hover:text-gray-700'} 
+                                            dark:border-gray-700 
+                                            dark:text-gray-400 
+                                            ${pageNumber == totalPages && 'cursor-not-allowed'}
+                                            `
+                                            }>
                                             <span className="sr-only">Next</span>
                                             <svg className="w-2.5 h-2.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
